f60b52e5eda2753301f2515d5e0cc60c29424e5e
Felix Jeckel <17598541+watnFoehn@users.noreply.github.com>
2023-12-15 13:57:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
 require("dotenv").config();
 
 const users = [];
+
+const exercises = [];
 
 app.use(cors());
 app.use(express.json());
@@ -18,8 +19,8 @@ app.get("/", (req, res) => {
 
 app.post("/api/users", (req, res) => {
   const { username } = req.body;
-  const userId = Math.random();
-  const user = { id: userId, username };
+  const userId = Math.floor(Math.random() * 900) + 100;
+  const user = { _id: userId, username };
 
   users.push(user);
   res.status(201).json(user);
@@ -27,6 +28,37 @@ app.post("/api/users", (req, res) => {
 
 app.get("/api/users", (req, res) => {
   res.json(users);
+});
+
+app.post("/api/users/:_id/exercises", (req, res) => {
+  const id = req.params._id;
+  const { description, duration, date } = req.body;
+  const calculatedDate = date ? date : new Date();
+  const exercise = {
+    _id: id,
+    description,
+    duration,
+    date: calculatedDate.toDateString(),
+  };
+
+  exercises.push(exercise);
+  res.status(201).send(JSON.stringify(exercise));
+});
+
+app.get("/api/users/:_id/exercises", (req, res) => {
+  const { _id } = req.params;
+  const user = users.find((user) => user._id.toString() === _id);
+  if (!user) {
+    res.status(404).json({ error: "User not found." });
+    return;
+  }
+  const userExercises = exercises.filter((exercise) => exercise._id === _id);
+  const responseData = {
+    ...user,
+    ...userExercises[0],
+  };
+
+  res.json(responseData);
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
